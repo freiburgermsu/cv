7755253abec7259f7df62fd4b485273be21e6ae6
Andrew Freiburger <freiburgermsu@aol.com>
2023-04-16 22:11:06 -0500

    Publications and Conferences nav tabs

--- header.js
+++ header.js
@@ -15,6 +15,8 @@ class Header extends HTMLElement {
         <ul>
           <li><a href="../index.html">Home</a></li>
           <li><a href="../CV.html">CV</a></li>
+          <li><a href="../CV/publications.html">Publications</a></li>
+          <li><a href="../CV/conferences.html">Conferences</a></li>
           <li><a href="../Reviews.html">Reviews</a></li>
         </ul>
       </nav>
